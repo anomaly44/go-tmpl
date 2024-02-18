c6f9170ab11dc9f2a993a26399ebd98367f2ca89
Rob Indesteege <rob.indesteege@jane.be>
2024-02-18 19:36:48 +0100

    add session auth

--- cmd/main.go
+++ cmd/main.go
@@ -4,17 +4,36 @@ import (
 	"fmt"
 
 	"github.com/anomaly44/go-tmpl/handler"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+// In production, the secret key of the CookieStore
+// and database name would be obtained from a .env file
+const (
+	SECRET_KEY string = "secret"
+	DB_NAME    string = "app_data.db"
 )
 
 func main() {
 	app := echo.New()
 
 	userHandler := handler.UserHandler{}
+
+	// logger middleware
+	app.Use(middleware.Logger())
+
+	// Session middleware
+	app.Use(session.Middleware(sessions.NewCookieStore([]byte(SECRET_KEY))))
+
 	app.Static("/static", "static")
 	app.GET("/", userHandler.HandleLoginShow)
 	app.POST("/login", userHandler.HandleLoginSubmit)
-	app.GET("/user", userHandler.HandleUserShow)
+
+	proctectedGroup := app.Group("/users", userHandler.AuthMiddleware)
+	proctectedGroup.GET("", userHandler.HandleUserShow)
 	fmt.Println("It is working")
 
 	app.Start(":8080")
--- handler/user.go
+++ handler/user.go
@@ -5,7 +5,17 @@ import (
 
 	"github.com/anomaly44/go-tmpl/model"
 	"github.com/anomaly44/go-tmpl/view/user"
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
+)
+
+const (
+	auth_sessions_key string = "authenticate-sessions"
+	auth_key          string = "authenticated"
+	user_id_key       string = "user_id"
+	username_key      string = "username"
+	tzone_key         string = "time_zone"
 )
 
 type UserHandler struct{}
@@ -24,15 +34,55 @@ func (h UserHandler) HandleLoginShow(c echo.Context) error {
 func (h UserHandler) HandleLoginSubmit(c echo.Context) error {
 	email := c.FormValue("email")
 	password := c.FormValue("password")
+	// obtaining the time zone from the POST request of the login form
+	tzone := ""
+	if len(c.Request().Header["X-Timezone"]) != 0 {
+		tzone = c.Request().Header["X-Timezone"][0]
+	}
+
 	formData := model.LoginFormData{
 		Values: map[string]string{"email": email, "password": password},
 	}
 	// print formdata
 	fmt.Printf("formdata is %v \n", formData)
 	if email == "rob@fixtrack.be" {
+		// Get Session and setting Cookies
+		sess, _ := session.Get(auth_sessions_key, c)
+		sess.Options = &sessions.Options{
+			Path:     "/",
+			MaxAge:   3600, // in seconds
+			HttpOnly: true,
+		}
+
+		// Set user as authenticated, their username,
+		// their ID and the client's time zone
+		sess.Values = map[interface{}]interface{}{
+			auth_key: true,
+			// user_id_key:  user.ID,
+			user_id_key:  "777777",
+			username_key: email,
+			tzone_key:    tzone,
+		}
+		sess.Save(c.Request(), c.Response())
+
 		// redirect to user page
-		return c.Redirect(302, "/user")
+		return c.Redirect(302, "/users")
 	}
 	fmt.Printf("email is %s and password is %s \n", email, password)
 	return render(c, user.Login(formData))
+}
+
+func (h UserHandler) AuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		sess, _ := session.Get(auth_sessions_key, c)
+		// if sess.Values[auth_key] == nil {
+		// 	return c.Redirect(302, "/")
+		// }
+		if auth, ok := sess.Values[auth_key].(bool); !ok || !auth {
+			fmt.Println(ok, auth)
+
+			return echo.NewHTTPError(echo.ErrUnauthorized.Code, "Please provide valid credentials")
+		}
+		return next(c)
+	}
 }
